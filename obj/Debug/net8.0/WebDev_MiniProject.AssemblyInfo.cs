d0e7e3a0145e8835f33d17f4302a4ec0b7ba5f5f
Eakomteen23 <eak555baba@gmail.com>
2024-10-01 22:12:46 +0700

    1

--- obj/Debug/net8.0/WebDev_MiniProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebDev_MiniProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a0e2f433ce85403f0ee7250f7d459d791ada49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5459ccdec64336f5d8f801c0630222c3350980a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/WebDev_MiniProject.AssemblyInfo.cs
+++ obj/Release/net8.0/WebDev_MiniProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d58de7249cca83c7b078de2e073edaf6749d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5459ccdec64336f5d8f801c0630222c3350980a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
