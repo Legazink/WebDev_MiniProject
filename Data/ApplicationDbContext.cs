21b50f43e34997039c6356a9b02afb732c6c81fc
Legazink <66015134@kmitl.ac.th>
2024-10-01 22:01:54 +0700

    AddAccountID

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,20 @@ namespace WebDev_MiniProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
         public DbSet<Post> Posts { get; set; }
+
+        // Override the OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Add custom configurations or constraints here
+            builder.Entity<Account>()
+                .Property(u => u.AccountID)
+                .ValueGeneratedOnAdd(); // Automatically increment AccountId when a new user is created
+
+            // You can add other configurations for the entities here as well
+        }
     }
 }
