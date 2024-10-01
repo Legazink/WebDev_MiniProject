9e31f4b56455b4bc72b40b3c932baf0eb2b7447d
Eakomteen23 <eak555baba@gmail.com>
2024-10-01 22:21:11 +0700

    picture name

--- obj/Release/net8.0/WebDev_MiniProject.AssemblyInfo.cs
+++ obj/Release/net8.0/WebDev_MiniProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5459ccdec64336f5d8f801c0630222c3350980a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad98c1a3ea9cd46d9bf05a949be7c348e5c2be98")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
