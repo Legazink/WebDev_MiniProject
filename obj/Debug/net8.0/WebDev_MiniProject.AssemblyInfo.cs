8fcacc276c3ab37035c4c2588386b8ec6f363ec2
Legazink <66015134@kmitl.ac.th>
2024-10-06 09:09:59 +0700

    new2

--- obj/Debug/net8.0/WebDev_MiniProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebDev_MiniProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2e831b84964df1493b7307928160f4418f91b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147e4acdacdca382cf8d157d4c7daf980438a346")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDev_MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
